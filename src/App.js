5a620d3b5355822636cadbd88511c5cccde753ef
Marissa Cadavid <cadavidbernsmarissa@johndeere.com>
2020-10-16 09:36:38 -0500

    Initial state and auth setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Header, Footer, Friend List, Profile, Posts
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      user: {
+        username: "CRHarding",
+        password: "Casey123",
+        profileImg: "https://www.denverpost.com/wp-content/uploads/2017/01/thor-the-dark-world.jpg",
+        posts: [],
+        friends: []
+      },
+      loggedIn: false,
+      error: ""
+    }
+  }
+
+  logIn(evt, userData){
+    evt.preventDefault();
+    const user = this.state.user;
+    if(userData.username === user.username && userData.password === user.password){
+      this.setState({
+        loggedIn: true,
+        error: ""
+      })
+    } else {
+      this.setState({
+        error: "Incorrect credentials"
+      })
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <h1>Hello World</h1>
+          <button>Log in / Register</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
